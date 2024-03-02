9cab0128aaeece1c35edc04f63edaddac562c608
yerp159 <101734432+yerp159@users.noreply.github.com>
2024-03-02 02:23:31 -0500

    classwork for 2/27

--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -1,3 +1,25 @@
+function sum2DArray(arr) {
+    return new Promise((resolve, reject) => {
+        if(Array.isArray(arr)) {
+            setTimeout(() => {
+                let sum = 0;
+                for (let i = 0; i < arr.length; i++) {
+                    for (let j = 0; j < arr[i].length; j++) {
+                        sum += arr[i][j];
+                    }
+                }
+                console.log('resolving ... ');
+                resolve(sum);
+            }, 0);
+        }
+        else {
+            console.log('rejecting ... ');
+            reject('BAD INPUT: Expected array as input');
+        }
+        console.log('returning from sum');
+    });
+}
+
 const array2D = [
     [1, 2, 3],
     [4, 5, 6],
